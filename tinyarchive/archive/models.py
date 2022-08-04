2654c4b2d9e16fda1d5601d57fa40170f9af6216
Lauren Trankiem <ltrankiem@berkeley.edu>
2022-08-03 23:43:13 -0700

    add somethin

--- tinyarchive/archive/models.py
+++ tinyarchive/archive/models.py
@@ -13,6 +13,7 @@ class ArchiveDocument(models.Model):
             return self.id
     objects=InheritanceManager()
     name = models.CharField(max_length=200)
+    something = models.CharField(max_length = 200)
     description = models.TextField(blank=True, null=False)
     photo_image = StdImageField(
         upload_to="photographs/",
